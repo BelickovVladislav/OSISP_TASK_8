4537c2d90d0aa36111caf4240f2081ebcd086c7b
Belikov Vlad <belickov.vlad1997@yandex.ru>
2018-06-27 14:43:45 +0300

    done

--- OSISP_TASK_8/Form1.cs
+++ OSISP_TASK_8/Form1.cs
@@ -12,9 +12,22 @@ namespace OSISP_TASK_8
 {
     public partial class Form1 : Form
     {
+        private Graphics g;
+        private Pen pen;
         public Form1()
         {
+
             InitializeComponent();
+            g = this.CreateGraphics();
+            pen = new Pen(Color.Black);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g.Clear(Color.White);
+            g = this.CreateGraphics();
+
+            g.DrawEllipse(pen, 0, 0, this.Width - 17, this.Height - 40);
         }
     }
 }
